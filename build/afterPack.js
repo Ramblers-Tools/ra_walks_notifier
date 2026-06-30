8bc35990613818b443fae2ed5984596a776cf01f
Richard Higham <richard.higham@ramblerseastcheshire.org.uk>
2026-06-30 10:53:38 +0100

    Strengthen mac metadata cleanup hook

--- build/afterPack.js
+++ build/afterPack.js
@@ -1,9 +1,18 @@
 const { execFileSync } = require("node:child_process");
+const { existsSync } = require("node:fs");
 
-exports.default = async function afterPack(context) {
+module.exports = async function afterPack(context) {
   if (context.electronPlatformName !== "darwin") {
     return;
   }
 
-  execFileSync("xattr", ["-cr", context.appOutDir], { stdio: "inherit" });
+  const appPath = `${context.appOutDir}/${context.packager.appInfo.productFilename}.app`;
+  const target = existsSync(appPath) ? appPath : context.appOutDir;
+
+  console.log(`Clearing macOS extended metadata from ${target}`);
+  execFileSync("find", [target, "-name", "._*", "-delete"], { stdio: "inherit" });
+  execFileSync("find", [target, "-name", ".DS_Store", "-delete"], { stdio: "inherit" });
+  execFileSync("dot_clean", ["-m", target], { stdio: "inherit" });
+  execFileSync("xattr", ["-cr", target], { stdio: "inherit" });
+  execFileSync("find", [target, "-exec", "xattr", "-c", "{}", ";"], { stdio: "inherit" });
 };
