a7c5ef1e12d83ccb40335717447ded27ced19e5c
Richard Higham <richard.higham@ramblerseastcheshire.org.uk>
2026-07-01 07:03:09 +0100

    Build Linux releases from current branch

--- scripts/build-linux-on-vm.sh
+++ scripts/build-linux-on-vm.sh
@@ -7,13 +7,16 @@ cd "$ROOT_DIR"
 LINUX_BUILD_HOST=${LINUX_BUILD_HOST:-10.10.10.68}
 LINUX_BUILD_USER=${LINUX_BUILD_USER:-richard}
 LINUX_BUILD_REPO=${LINUX_BUILD_REPO:-/home/richard/Documents/code/ra_walks_notifier}
+LINUX_BUILD_BRANCH=${LINUX_BUILD_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 VERSION=$(node -p "require('./package.json').version")
 REMOTE="$LINUX_BUILD_USER@$LINUX_BUILD_HOST"
 
 ssh "$REMOTE" "
   set -euo pipefail
   cd '$LINUX_BUILD_REPO'
-  git pull origin main
+  git fetch origin '$LINUX_BUILD_BRANCH'
+  git checkout '$LINUX_BUILD_BRANCH'
+  git reset --hard 'origin/$LINUX_BUILD_BRANCH'
   npm ci
   npm run release:linux
 "
